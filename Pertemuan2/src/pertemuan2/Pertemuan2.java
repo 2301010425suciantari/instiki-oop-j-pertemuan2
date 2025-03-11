64b2c128ea8477e91dadae7a3d543adf4b6f45c2
PC-F-04 <suciantari46@gmail.com>
2025-03-11 21:55:37 +0800

    Pertemuan 2

---
+++ Pertemuan2/src/pertemuan2/Pertemuan2.java
@@ -0,0 +1,25 @@
+package pertemuan2;
+
+/**
+ *
+ * @author suciantari
+ * tgl : 11 maret 2025
+ */
+public class Pertemuan2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nama = "Suciantari";
+        String nim = "2301010425";
+        System.out.println("pertemuan 2 java");
+        System.out.println("~~~~~~~~~~~~~~~~");
+        System.out.println("Nama    : "+nama);
+        System.out.println("NIM     : "+nim);
+        System.out.printf("Nama Saya %s dengan NIM %s, saat ini sedang belajar pemrograman bahasa java",nama,nim);
+        
+      
+    }
+    
+}
